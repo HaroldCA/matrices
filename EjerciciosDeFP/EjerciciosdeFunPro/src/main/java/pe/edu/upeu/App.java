4ab9b758f2ac0a38358fcb61d37c31fcdaa8de4c
HTCA DLCAyYCAU <harold_t2003@hotmail.com>
2021-07-22 22:39:50 -0500

    primer commit

---
+++ EjerciciosDeFP/EjerciciosdeFunPro/src/main/java/pe/edu/upeu/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void TransformarM05( String[] args ){
+        System.out.println("");
+    }
+    
+}
